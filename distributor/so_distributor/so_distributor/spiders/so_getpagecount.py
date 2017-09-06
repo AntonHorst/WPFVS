e443f97f50fdeb923af2ba7d61ac498a3e7356dd
Webcrawler AVS <webcrawler@pip10.inf.FH-Koeln.DE>
2017-09-06 16:06:51 +0200

    Crawler der die Gesamtanzahl an Seiten in eine Datei schreibt

---
+++ distributor/so_distributor/so_distributor/spiders/so_getpagecount.py
@@ -0,0 +1,23 @@
+import scrapy
+
+class PageCountSpider(scrapy.Spider):
+	name="pagecount"
+
+	def start_requests(self):
+		urls = [
+			'https://stackoverflow.com/questions'
+		]
+
+		for url in urls:
+			yield scrapy.Request(url=url, callback=self.parse)
+
+	def parse(self, response):
+		page_fl_selector= ".pager"
+		page_number_selector = ".page-numbers ::text"
+		numberpart = response.css(page_fl_selector)
+			
+			#'count': numberpart.css(page_number_selector).extract()[6],
+		filename='count.txt'
+		with open(filename, 'w') as f:
+			f.write(numberpart.css(page_number_selector).extract()[6])
+	
