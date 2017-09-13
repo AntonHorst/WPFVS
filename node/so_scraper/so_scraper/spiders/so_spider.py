b5e627879215ee1e08531e15fee446db1d38c9b2
Webcrawler AVS <webcrawler@pip10.inf.FH-Koeln.DE>
2017-09-13 16:15:56 +0200

    Ausgabe des Scrapers in einzelne Items

--- node/so_scraper/so_scraper/spiders/so_spider.py
+++ node/so_scraper/so_scraper/spiders/so_spider.py
@@ -16,17 +16,18 @@ class so_spider(scrapy.Spider):
 	def parse(self, response):
 		QUESTION_SELECTOR = '.question-summary'
 		for question in response.css(QUESTION_SELECTOR):
-			l = ItemLoader(item=SoScraperItem(), response=response)
+			l = ItemLoader(item=SoScraperItem(), selector=question)
 			TAG_SELECTOR = '.post-tag ::text'
 			VIEW_SELECTOR = '.views ::text'
 			VOTE_SELECTOR = '.vote-count-post ::text'
 			ANSWER_SELECTOR = '.status answered ::text'
 			
 			l.add_css('tags', TAG_SELECTOR)
+			#l.add_value('tagAmount', len(question.css(TAG_SELECTOR)))
 			l.add_css('views', VIEW_SELECTOR)
 			l.add_css('votes', VOTE_SELECTOR)
 			l.add_css('answers', ANSWER_SELECTOR)
-			return l.load_item()
+			yield l.load_item()
 			"""	
 			yield{
 				'views': question.css(VIEW_SELECTOR).extract(),
