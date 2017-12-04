0419a82844ccafce909250c20208c52fb474c26a
elder92 <webcrawler@pip10.inf.FH-Koeln.DE>
2017-12-04 11:47:05 +0100

    Delay Midlleware

--- node/so_scraper/so_scraper/middlewares.py
+++ node/so_scraper/so_scraper/middlewares.py
@@ -6,7 +6,20 @@
 # http://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
+from scrapy.downloadermiddlewares.retry import RetryMiddleware
+from scrapy.utils.response import response_status_message
+from time import sleep
 
+class SleepRetryMiddleware(RetryMiddleware):
+	def __init__(self, settings):
+		RetryMiddleware.__init__(self, settings)
+
+	def process_response(self, request, response, spider):
+		if response.status in [403,500, 429, 503]:
+			sleep(120)
+			reason = response_status_message(response.status)
+			return self._retry(request, reason, spider) or response
+		return super(SleepRetryMiddleware, self).process_response(request, response, spider)
 
 class SoScraperSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
