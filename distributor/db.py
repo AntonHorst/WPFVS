1b5ffa94619b1c61888d143a76bdbb7f846cb6f7
Webcrawler AVS <webcrawler@pip09.inf.FH-Koeln.DE>
2017-12-06 13:37:58 +0100

    DB Wrapper Insert Befehl

--- distributor/db.py
+++ distributor/db.py
@@ -17,13 +17,16 @@ class DB:
 		self.connection.close()
 
 	def insert(self, tag, vote, view, answer):
-		sqlCommand = """INSERT INTO stack (tag, votes, views, asnwers) 
-		VALUES (""" + tag + """, """ + str(vote) + """, """ + str(view) + """, """ + str(answer) + """) ON DUPLICATE KEY
-		UPDATE view = (SELECT views FROM stack WHERE tag = """ + tag + """) + """ + str(view) + """,
-		vote = (SELECT votes FROM stack WHERE tag = """ + tag + """) + """ + str(vote) + """,
-		answer = (SELECT answers FROM stack WHERE tag = """ + tag + """) + """ + str(answer) + """;"""
+		#sqlCommand = """INSERT INTO stack (tag, votes, views, asnwers) 
+		#VALUES (""" + tag + """, """ + str(vote) + """, """ + str(view) + """, """ + str(answer) + """) ON DUPLICATE KEY
+		#UPDATE view = (SELECT views FROM stack WHERE tag = """ + tag + """) + """ + str(view) + """,
+		#vote = (SELECT votes FROM stack WHERE tag = """ + tag + """) + """ + str(vote) + """,
+		#answer = (SELECT answers FROM stack WHERE tag = """ + tag + """) + """ + str(answer) + """;"""
+		
+		sqlCommand = """INSERT OR REPLACE INTO stack (tag, votes, views, answers) VALUES ('"""+ tag +"""', (SELECT CASE  WHEN exists(SELECT 1  FROM stack WHERE tag='"""+ tag + """')THEN (SELECT votes FROM stack WHERE tag='"""+ tag + """') + """ + str(vote) + """  ELSE """ + str(vote) + """  END ), (SELECT CASE WHEN exists (SELECT 1 FROM stack WHERE tag='"""+ tag + """') THEN (SELECT views FROM stack WHERE tag='"""+ tag + """') + """+str(view)+ """ ELSE + """+str(view)+ """ END),(SELECT CASE WHEN exists (SELECT 1 FROM stack WHERE tag ='"""+ tag + """') THEN (SELECT answers FROM stack WHERE tag='"""+ tag + """') + """ + str(answer) + """ ELSE """ + str(answer) + """ END));"""
+
 		print (sqlCommand)
 		#sleep(100)
-		
+				
 		self.cursor.execute(sqlCommand)
 		self.connection.commit()
